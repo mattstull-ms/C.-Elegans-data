9da19755f55c17c5db8d91ca715ce561fa1af7cd
mattstull-ms <mattstull.ms@tamu.edu>
2017-03-15 11:22:16 -0500

    Update ui.r

--- QC-App-2.0/ui.r
+++ QC-App-2.0/ui.r
@@ -45,6 +45,7 @@ shinyUI(navbarPage("QC App",
                               tabPanel("K-fold Validation",tags$div(id = "validation1")),
                              tabPanel("Variable Importance",tags$div(id = "variableimportance1")),
                              tabPanel("Diagnostics", tags$div(id="Plot_1"))
+                             
                             
                             )
                    ),            
@@ -72,8 +73,38 @@ shinyUI(navbarPage("QC App",
                               )
                               
                             )
-                            
-                   )
+                   ),
+                   tabPanel("Citations",
+                            h3("References"),
+                            p("Winston Chang, Joe Cheng, JJ Allaire, Yihui Xie and Jonathan McPherson (2017). shiny: Web Application Framework for R. R package version
+                              1.0.0. https://CRAN.R-project.org/package=shiny"),
+                            p("Max Kuhn. Contributions from Jed Wing, Steve Weston, Andre Williams, Chris Keefer, Allan Engelhardt, Tony Cooper, Zachary Mayer, Brenton
+                              Kenkel, the R Core Team, Michael Benesty, Reynald Lescarbeau, Andrew Ziem, Luca Scrucca, Yuan Tang, Can Candan and Tyler Hunt. (2016).
+                              caret: Classification and Regression Training. R package version 6.0-73. https://CRAN.R-project.org/package=caret"),
+                            p("Jonathan Owen (2016). rhandsontable: Interface to the 'Handsontable.js' Library. R package version 0.3.4.
+                              https://CRAN.R-project.org/package=rhandsontable"),
+                            p(" Hui Zou and Trevor Hastie (2012). elasticnet: Elastic-Net for Sparse Estimation and Sparse PCA. R package version 1.1.
+                              https://CRAN.R-project.org/package=elasticnet"),
+                            p("Alexandros Karatzoglou, Alex Smola, Kurt Hornik, Achim Zeileis (2004). kernlab - An S4 Package for Kernel Methods in R. Journal of
+                              Statistical Software 11(9), 1-20. URL http://www.jstatsoft.org/v11/i09/"),
+                            p("David Meyer, Evgenia Dimitriadou, Kurt Hornik, Andreas Weingessel and Friedrich Leisch (2017). e1071: Misc Functions of the Department of
+                              Statistics, Probability Theory Group (Formerly: E1071), TU Wien. R package version 1.6-8. https://CRAN.R-project.org/package=e1071"),
+                            p("Stephen Milborrow. Derived from mda:mars by Trevor Hastie and Rob Tibshirani. Uses Alan Miller's Fortran utilities with Thomas Lumley's
+                              leaps wrapper. (2017). earth: Multivariate Adaptive Regression Splines. R package version 4.4.9. https://CRAN.R-project.org/package=earth"),
+                            p("Revolution Analytics and Steve Weston (2015). doMC: Foreach Parallel Adaptor for 'parallel'. R package version 1.3.4.
+                              https://CRAN.R-project.org/package=doMC"),
+                            p("Thomas Grubinger, Achim Zeileis, Karl-Peter Pfeiffer (2014). evtree: Evolutionary Learning of Globally Optimal Classification and
+                              Regression Trees in R. Journal of Statistical Software, 61(1), 1-29. URL http://www.jstatsoft.org/v61/i01/."),
+                            p("H. Deng(2013). Guided Random Forest in the RRF Package. arXiv:1306.0237."),
+                              
+                              p("H. Deng and G. Runger (2013). Gene Selection with Guided Regularized Random Forest. Pattern Recognition 46(12): 3483-3489."),
+                              
+                              p("H. Deng and G. Runger (2012). Feature Selection via Regularized Trees. The 2012 International Joint Conference on Neural Networks (IJCNN)."),
+                            p("Nicolai Meinshausen (2016). quantregForest: Quantile Regression Forests. R package version 1.3-5.
+                              https://CRAN.R-project.org/package=quantregForest"),
+                            p("  Tianqi Chen, Tong He, Michael Benesty, Vadim Khotilovich and Yuan Tang (2017). xgboost: Extreme Gradient Boosting. R package version 0.6-4.
+                              https://CRAN.R-project.org/package=xgboost")
+                            )
                    
 )
 )
